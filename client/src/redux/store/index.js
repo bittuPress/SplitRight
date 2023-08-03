1158af853eb66225ae421bd68c04c922aba5e8e7
subham <freelancerrsubham@gmail.com>
2023-08-03 08:03:51 +0545

    Persist Redux Setup

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,15 +1,21 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { combineReducers } from 'redux'
+import storage from 'redux-persist/lib/storage';
+import { persistReducer, persistStore } from 'redux-persist';
 import logger from 'redux-logger' 
-
 import users from '../reducerSlice/Users'
+
+const persistConfig = {
+    key: 'root',
+    storage,
+}
 const reducer = combineReducers({
     users,
 })
-
-const store = configureStore({
-     reducer, 
+const persistedReducer = persistReducer(persistConfig, reducer)
+export const store = configureStore({
+     reducer: persistedReducer,
      middleware:[logger]
 })
 
-export default store;
+export const persistor = persistStore(store);
